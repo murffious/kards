457eeb9514ebe2f8cd8ae01cf32f01787d794307
murffious <titan.murff@gmail.com>
2018-07-12 17:01:28 -0600

    more notes good stuff

--- react-ui/src/constants/Content_Routes.js
+++ react-ui/src/constants/Content_Routes.js
@@ -41,7 +41,7 @@ Using techincal langauge like CEo codesmith taught
 
 
 Dev Skill: Be able to use version control to develop 
-
+maintain code already written make imporvements fix bugs 
 the main thing needed is a reference guide so how can that best be done
 
 kinda like a checklist to back up the project based learning 
@@ -58,3 +58,380 @@ vocab throughout
 Project: 
 
 Time is a huge issue:  what to study out side of class
+
+asking the right questions like that guide
+
+the get the student to arrive at that moment-- 
+USING IT but also understanding it 
+
+Chapter `1`
+build a web design skeleton using HTML/CSS based off of an image 
+application:  Working for a company where you are given a design 
+and you are to bring it to life 
+
+Waka Timing for Coidgn time versus learning time 80 20 and timer 
+
+clean coding reminder and coding skills liek debugging and so forth enforce
+Header
+   * Nav
+   * Aside (With a Heading)
+   * Section (With a Heading)
+   * Article (With a Heading) contained within the Section
+   * Headers contained within the Article
+   * Paragraph contained within the Article
+   
+   * `float`, `padding`, `margin`, `display`, `overflow`, `clear` and `text-align`.
+  
+   * HW Activites to help you do this HW 
+
+   * Activity 1-2 Terminal mastery 
+   * Activities 3-7 - Basic HTML and CSS - how to make and style an html document 
+   * Activities 8-11 CSS positions and making a layout: CSS positioning : relative, absolute fixed , 
+   * PRO TIPS and Best Practices
+   * Layout Hint: make a skeleton of the whole site using placeholder images and background colors to just show where each piece will go to get the spacing right.
+   
+
+   TOOLS to do that : knowledge to do so. 
+   Give definiation or not the amount of inof and how to much to give is off
+
+
+   What are all these tags used for and what attibutras and syntax do they have.
+   why or where do they fit into my project. 
+
+   CODE 
+CODING building stuff - 
+exercises like drills 
+reading code dissect - 
+vocab/sytnax learning 
+concepts exploration
+best practices and design patterns
+
+
+programmer skills
+debugging
+reading documations 
+learning quickly 
+tools mastery
+Problem solving
+understaning Computers 
+comptuer Science Concpets 
+problem solving
+and the internet etc
+
+copy paste and change data and stuff to meet your needs 
+solve problems 
+
+   JavaScript
+   1
+   Declaring - javascript var vocab
+   
+   ❓ Variables can be declared in JavaScript using the ____ keyword.
+   
+   var ✅
+   value
+   val
+   variable
+   2
+   Variable Types - javascript var boolean string number vocab
+   
+   ❓ Commonly used data types DO NOT include:
+   
+   strings
+   booleans
+   alerts ✅
+   numbers
+   3
+   Strings - javascript string vocab
+   
+   ❓ String values must be enclosed within ____ when being assigned to variables.
+   
+   commas
+   curly brackets
+   quotes ✅
+   parentheses
+   4
+   Debugging - javascript console.log vocab
+   
+   ❓ A very useful tool used during development and debugging for printing content to the debugger is:
+   
+   JavaScript
+   terminal / bash
+   for loops
+   console.log ✅
+   5
+   Adding variables - javascript var adding
+   
+   ❓ What is the value of c in the following code?
+   
+   var a = 3;
+   var b = '3';
+   var c = a + b;
+   6
+   '33' ✅
+   ab
+   This will cause an error
+   6
+   User Input - javascript prompt vocab
+   
+   ❓ We can ask a user for their name by using ____ popup box in the browser.
+   
+   a confirm
+   an alert
+   an ask
+   a prompt ✅
+   7
+   Writing to a Page - javascript vocab document.write
+   
+   ❓ We can use JavaScript to directly write to the HTML page itself by using:
+   
+   console.log
+   document.write ✅
+   variables
+   This is not possible with JavaScript
+   8
+   If / Else Statements - if/else javascript vocab
+   
+   ❓ If / Else statements are best suited for ____.
+   
+   making decisions ✅
+   setting up arrays
+   interacting with users
+   performing calculations
+   9
+   If / Else Syntax - if/else javascript syntax
+   
+   ❓ The condition in an if / else statement is enclosed within ____.
+   
+   quotes
+   curly brackets
+   parentheses ✅
+   square brackets
+   10
+   Arrays - javascript arrays vocab
+   
+   ❓ Arrays in JavaScript can be used to store ____.
+   
+   numbers and strings
+   other arrays
+   booleans
+   all of the above ✅
+   11
+   Array elements - javascript arrays index vocab
+   
+   ❓ The specific element stored within an array can be targeted using the element ____.
+   
+   value
+   index ✅
+   location
+   length
+   12
+   Getting Arrray Values - javascript arrays index syntax
+   
+   ❓ What is the value of myValue in the code below?
+   
+       var myArray = ["dog", "cat", "mouse"];
+       var myValue = myArray[1];
+   1
+   dog
+   cat ✅
+   mouse
+   13
+   Total Array Elements - javascript arrays array.length vocab
+   
+   ❓ We can get the number of elements in an array using the ____ property.
+   
+   count
+   size
+   variable
+   length ✅
+   14
+   Repeating Code - javascript for loops vocab
+   
+   ❓ We can run repeated blocks of code using ____.
+   
+   for loops ✅
+   if / else statements
+   arrays
+   browser refresh
+   15
+   Comparing Variables - javascript comparison vocab
+   
+   ❓ We can check to see if two values are equal using ____.
+   
+   ?==
+   !==
+   === ✅
+   =
+   16
+   For Loop Counter Variables - javascript for loops iterators vocab
+   
+   ❓ The counter variable in a for loop is also known as the ____.
+   
+   iterator ✅
+   condition
+   return
+   block
+   17
+   Looping Through Arrays - javascript arrays for loops array.length index
+   
+   ❓ What is the value of myValue after running the code below?
+   
+       var myValue = 0;
+       var myArray = [1, 2, 3, 4];
+       for (var i = 1; i < myArray.length; i++) {
+           myValue += myArray[i];
+       }
+   5
+   6
+   9 ✅
+   10
+   18
+   Hooking onto Events - javascript vocab document.onkeyup
+   
+   ❓ We can hook onto the event of a user pressing and releasing a key by referring to ____.
+   
+   browser.event
+   page.hit
+   event.key
+   document.onkeyup ✅
+   19
+   Getting Key Presses - javascript vocab event.key
+   
+   ❓ We can get the key a user has pressed by referencing ____.
+   
+   document.onkeyup
+   letter.pressed
+   keyboard.event
+   event.key ✅
+   20
+   ID Element Targetting - javascript document.getElementByID vocab
+   
+   ❓ We can find a single element on the page that has an ID by using ____.
+   
+   browser.findAllElements
+   document.getElementById ✅
+   clicked.getHTMLById
+   document.ElementsByClass
+   21
+   Adding to Arrays - javascript vocab arrays push
+   
+   ❓ Elements can be added to an array by using the ____ method.
+   
+   push ✅
+   add
+   concatenate
+   attach
+   22
+   Bundling Repetitive Code - javascript vocab functions
+   
+   ❓ Repetitive lines of code should be bundled together as ____ and then executed as needed.
+   
+   arrays
+   functions ✅
+   snippets
+   conditions
+   23
+   Variables Passed to Functions - javascript vocab functions arguments
+   
+   ❓ A function's ____ are only visible inside the function body.
+   
+   arguments ✅
+   arrays
+   strings
+   definitions
+   24
+   String Manipulation - javascript vocab syntax charAt
+   
+   ❓ To grab the first letter of a string we can use the ____ method.
+   
+   stringItem(0)
+   itemIs(1)
+   charAt(0) ✅
+   indexOf(1)
+   25
+   Building Arrays - javascript arrays push modulus
+   
+   ❓ What will the following code log to the console?
+   
+       var myArray = [];
+   
+       for (var i = 0; i < 10; i++) {
+           if (i % 2 == 0) {
+               myArray.push(i);
+           }
+       }
+   
+       console.log(myArray);
+   [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+   [2, 4, 6, 8, 10]
+   [1, 3, 5, 7, 9]
+   [0, 2, 4, 6, 8] ✅
+   26
+   Executing Functions - javascript vocab functions
+   
+   ❓ In order to actually execute a function we have written, we have to ____ it.
+   
+   run
+   call ✅
+   maximize
+   mark
+   27
+   Converting Strings - javascript vocab toLowerCase()
+   
+   ❓ We can convert "HELLO" to "hello" using the ____ method.
+   
+   reduceString()
+   toLowerCase() ✅
+   allLower()
+   minimizeIt()
+   28
+   Random Number Generation - javascript vocab Math.floor Math.random
+   
+   ❓ In the code below, the possible values of x are:
+   
+       function randomValue() {
+           var value = Math.floor(Math.random() * 5);
+           return value;
+       }
+   
+       var x = randomValue();
+   0, 1, 2, 3, 4 ✅
+   0, 1, 2, 3, 4, 5
+   1, 2, 3, 4
+   1, 2, 3, 4, 5
+   29
+   Function Scope 1 - javascript functions scope
+   
+   ❓ What will be logged to the console with the following code:
+   
+       var value = 33;
+   
+       function myFunction() {
+           value = 66;
+       }
+   
+       myFunction();
+   
+       console.log(value);
+   33
+   66 ✅
+   99
+   None of the above - this will cause an error
+   30
+   Function Scope 2 - javascript functions scope
+   
+   ❓ What will be logged to the console with the following code:
+   
+       var value = 33;
+   
+       function myFunction() {
+           var value = 66;
+       }
+   
+       myFunction();
+   
+       console.log(value);
+   33 ✅
+   66
+   99
+   None of the above - this will cause an error
